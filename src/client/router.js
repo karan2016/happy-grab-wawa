1b7c006ceac66a81cea666981f6acf1cdf2bf6d2
ZhiChen <karan.shaw@outlook.com>
2017-11-09 02:34:18 +0800

    Add red purse pages & styles

--- src/client/router.js
+++ src/client/router.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from 'containers/App';
 import Book from 'containers/Book';
+import RedPurse from 'containers/RedPurse';
 
 export default function () {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/book/:id" name="book" component={Book} />
+        <Route path="/red-purse/:id" name="rebPurse" component={RedPurse} />
         <Route path="/" name="home" component={App} />        
       </Switch>
     </BrowserRouter>
